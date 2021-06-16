ca879aa52d16138f9fa70959a430496c99b9c2a3
sravanthi-kotha20 <sravanthi.tech@gmail.com>
2021-06-16 12:37:23 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -36,6 +36,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform = new Ground(200,300,100,100)
+
 }
 
 function draw(){
@@ -62,4 +64,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
